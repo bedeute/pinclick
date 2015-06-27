f44e251c6b5c86105bd73e516611574c3d16d224
bedeute <bedeute@gmail.com>
2015-06-28 05:23:55 +0700

    update dashboard

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ opts.src = {
     app: [
       'bower_components/jquery/dist/jquery.js',
       'bower_components/bootstrap/js/**/*.js',
+      'bower_components/Flot/jquery.flot.js',
       'bower_components/pikaday/pikaday.js',
       'bower_components/pikaday/plugins/pikaday.jquery.js',
       'app/scripts/app.js'
