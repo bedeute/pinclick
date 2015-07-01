fb41cc634141f4cdd05ca632ab12ab8a0f3cf53f
bedeute <bedeute@gmail.com>
2015-07-01 17:09:28 +0700

    update project name to 'pinclick'

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,15 @@
 
     $('.datepicker').pikaday({ firstDay: 1 });
 
+
+    $('#ca-slider-toggle').click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.ca-body').toggleClass('shrink');
+    });
+
+
+
     var graphData = [{
             // Visits
             data: [ [6, 1300], [7, 1600], [8, 1900], [9, 2100], [10, 2500], [11, 2200], [12, 2000], [13, 1950], [14, 1900], [15, 2000] ],
